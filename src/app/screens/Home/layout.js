1861f3f60e6385a79bbd96b782da2d66a0fe8466
raparicio6 <rodrigoaparicio6@gmail.com>
2020-04-13 05:44:05 -0300

    card component finished

--- src/app/screens/Home/layout.js
+++ src/app/screens/Home/layout.js
@@ -12,12 +12,13 @@ function Home() {
     <div className={`row center background-wild-sand all-screen-height ${styles.container}`}>
       <div className="column m-right-10">
         <Entity imageUrl={monsterImage} hp={32} maxHp={40} name="Enemy" shield={0} />
-        <Entity hp={16} maxHp={48} name="Player" shield={45} className="m-top-6" />
-        <div className="row">
-          <Card entityType="player" type="damage" value={9} className="m-top-10 m-right-4" />
-          <Card entityType="player" type="shield" value={8} className="m-top-10 m-right-4" />
-          <Card entityType="player" type="damage" value={12} className="m-top-10 m-right-4" />
-          <Card entityType="player" type="heal" value={8} className="m-top-10" />
+        <Entity hp={16} maxHp={48} name="Player" shield={45} className="m-top-6 m-bottom-10" />
+        <div className={`row ${styles.cards}`}>
+          <Card entityType="player" type="damage" value={9} className="m-right-4" />
+          <Card entityType="player" type="shield" value={8} className="m-right-4" />
+          <Card entityType="player" type="damage" value={12} className="m-right-4" />
+          <Card entityType="player" type="heal" value={8} className="m-right-4" />
+          <Card entityType="player" type="shield" value={12} className="m-right-4" />
         </div>
       </div>
       <Turns current={12} left={8} past={11} />
