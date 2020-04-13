5bfb09fd465fae6f5d727cdcbf9b8ed7822edd98
raparicio6 <rodrigoaparicio6@gmail.com>
2020-04-13 04:53:21 -0300

    turns component

--- src/app/components/Turns/layout.js
+++ src/app/components/Turns/layout.js
@@ -6,12 +6,25 @@ import styles from './styles.module.scss';
 
 function Turns({ current, past, left }) {
   return (
-    <div className="column">
-      <span>{t('Turns:turns')}</span>
-      <span>{t('Turns:current')}</span>
-      <span>{t('Turns:past')}</span>
-      <span>{t('Turns:left')}</span>
-      <span>{t('Turns:endTurn')}</span>
+    <div className={`column ${styles.container}`}>
+      <div className={styles.turnsContainer}>
+        <span className={`row center ${styles.turns}`}>{t('Turns:turns')}</span>
+      </div>
+      <div className={styles.turnsInfoContainer}>
+        <div className={`column center ${styles.bordered}`}>
+          <span className={styles.info}>{t('Turns:current')}</span>
+          <span className={`${styles.info} ${styles.number} m-top-4`}>{current}</span>
+          <span className={`${styles.info} m-top-10`}>{t('Turns:past')}</span>
+          <span className={`${styles.info} ${styles.number} m-top-4`}>{past}</span>
+          <span className={`${styles.info} m-top-10`}>{t('Turns:left')}</span>
+          <span className={`${styles.info} ${styles.number} m-top-4`}>{left}</span>
+        </div>
+      </div>
+      <div className={`row center middle ${styles.endTurnContainer}`}>
+        <button type="button" className={styles.endTurnButton}>
+          {t('Turns:endTurn')}
+        </button>
+      </div>
     </div>
   );
 }
